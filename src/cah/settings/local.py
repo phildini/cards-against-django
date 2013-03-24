de1bdf46cfb84bcfb56743aa7d51d63e26b55318
Philip James <pjj@philipjohnjames.com>
2013-03-23 23:41:22 -0700

    Initial commit
    - basic requirements setup
    - basic project setup
    - CAH rules

---
+++ src/cah/settings/local.py
@@ -0,0 +1,21 @@
+# Local settings
+from .base import *
+
+DEBUG = True
+TEAMPLATE_DEBUG = DEBUG
+
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql_psycopg2',
+		'NAME': 'cah-django',
+		'USER': '',
+		'PASSWORD': '',
+		'HOST': 'localhost',
+		'PORT': '',
+	}
+}
+
+INSTALLED_APPS += ('debug_toolbar', )
+INTERNAL_IPS = ('127.0.0.1',)
+MIDDLEWARE_CLASSES += \
+            ('debug_toolbar.middleware.DebugToolbarMiddleware', )
