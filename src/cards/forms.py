87749895855628f76f7c527ddafdd27464a953ad
Philip James <pjj@philipjohnjames.com>
2013-03-24 20:40:52 -0700

    Now doing things more of the right way with the forms.

--- src/cards/forms.py
+++ src/cards/forms.py
@@ -1,13 +1,11 @@
 from django import forms
 from django.forms.widgets import RadioSelect
 
-CARDS = ((1, 'A bloody pacifier.'), (2, 'A Bop It.'), (3, 'Clearing a bloody path through Walmart with a scimitar.'))
-
 class PlayerForm(forms.Form):
-	card_selection = forms.MultipleChoiceField(required=True, widget=RadioSelect, choices=CARDS)
 
 	def __init__(self, *args, **kwargs):
-		cards = kwargs.pop('cards')
+		cards = kwargs.pop('cards', ())
+		super(PlayerForm, self).__init__(*args, **kwargs)
+		self.fields['card_selection'] = forms.MultipleChoiceField(required=True, widget=RadioSelect, choices=cards)
 
-		super(forms.Form, self).__init__(*args, **kwargs)
-		self.card_selection = forms.MultipleChoiceField(required=True, widget=RadioSelect, choices=cards)
+		
--- src/cards/views.py
+++ src/cards/views.py
@@ -1,17 +1,26 @@
 # Create your views here.
-from django.views.generic import TemplateView
+from django.views.generic import FormView
 from forms import PlayerForm
 
-class PlayerView(TemplateView):
+class PlayerView(FormView):
 
 	template_name = 'player.html'
+	selection = ''
+	cards = ((1, 'A bloody pacifier.'), (2, 'A Bop It.'), (3, 'Clearing a bloody path through Walmart with a scimitar.'))
+	form_class = PlayerForm
+
+
+	def get_success_url(self):
+		return '/admin'
 
 	def get_context_data(self, **kwargs):
-		context = super(TemplateView, self).get_context_data(**kwargs)
-		cards = {1:"one", 2:"two", 3:"three"}
-		context['cards'] = cards
-		form = PlayerForm(cards=cards)
-		context['form'] = form
+		context = super(FormView, self).get_context_data(**kwargs)
+		context['cards'] = self.cards
+		if self.selection:
+			context['select_card'] = self.selection
 		return context
 
-
+	def get_form_kwargs(self):
+		kwargs = super(FormView, self).get_form_kwargs()
+		kwargs['cards'] = self.cards
+		return kwargs
