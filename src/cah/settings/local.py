3cb4b526e1320c1a24367710d8895d115fc78c94
Philip James <philip@eventbrite.com>
2013-03-26 16:16:59 -0700

    Getting caching and cookies working in harmony.

--- src/cah/settings/local.py
+++ src/cah/settings/local.py
@@ -23,3 +23,10 @@ INSTALLED_APPS += ('debug_toolbar', )
 INTERNAL_IPS = ('127.0.0.1',)
 # MIDDLEWARE_CLASSES += \
 #             ('debug_toolbar.middleware.DebugToolbarMiddleware', )
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake'
+    }
+}
--- src/cards/views.py
+++ src/cards/views.py
@@ -7,16 +7,31 @@ import os
 from django.conf import settings
 from django.views.generic import FormView, TemplateView
 from django.core.urlresolvers import reverse
+from django.core.cache import cache
 from forms import PlayerForm
+from game import Game
 
 class PlayerView(FormView):
 
     template_name = 'player.html'
     form_class = PlayerForm
+    game_name = 'game1'
+
+    cards = ((1,'1'),(2, '2'))
 
     def __init__(self, *args, **kwargs):
-        self.read_player()
+        if not cache.get('global_blacks'):
+            self.game_init()
+        self.global_whites = cache.get('global_whites')
+        self.global_blacks = cache.get('global_blacks')
         super(PlayerView, self).__init__(*args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        self.create_game()
+        self.game = cache.get('games')[self.game_name]
+        self.request.session['player_name'] = 'player1'
+        self.read_player()
+        return super(PlayerView, self).dispatch(request, *args, **kwargs)
 
 
     def get_success_url(self):
@@ -24,32 +39,52 @@ class PlayerView(FormView):
 
     def get_context_data(self, **kwargs):
         context = super(PlayerView, self).get_context_data(**kwargs)
-        self.request.session['player_name'] = self.player['name']
+
         context['name_from_session'] = self.request.session.get('player_name', '')
         self.is_card_czar = context['is_card_czar'] = self.player['is_card_czar'] == 1
-        context['cards'] = self.player['hand']
-        context['player_name'] = self.player['name']
+        context['player_name'] = self.name
         context['selected'] = self.player['selected']
         return context
 
     def get_form_kwargs(self):
         kwargs = super(PlayerView, self).get_form_kwargs()
-        kwargs['cards'] = tuple(tuple(card) for card in self.player['hand'])
+        kwargs['cards'] = tuple((card, self.global_whites[card]) for card in self.player['hand'])
         return kwargs
 
     def form_valid(self, form):
         self.player['selected'] = form.cleaned_data['card_selection']
-        self.write_player()
+        # self.write_player()
         print form.cleaned_data['card_selection']
         return super(PlayerView, self).form_valid(form)
 
     def read_player(self):
-        with open(os.path.join(settings.PROJECT_ROOT, 'player.json')) as data:
-            self.player = json.loads(data.read())
+        self.name = self.request.session['player_name']
+        self.player = self.game['players'][self.name]
 
     def write_player(self):
         with open(os.path.join(settings.PROJECT_ROOT, 'player.json'), 'w') as data:
             data.write(json.dumps(self.player))
+
+    def game_init(self):
+        with open(os.path.join(settings.PROJECT_ROOT, 'data/data.json')) as data:
+            cards = json.loads(data.read())
+            cache.set('global_blacks', cards['black_cards'], 3600)
+            cache.set('global_whites', cards['white_cards'], 3600)
+
+    def create_game(self):
+        games = {
+            'game1':{
+                'players': {
+                    'player1': {
+                        'hand': [1,2,3],
+                        'is_card_czar': False,
+                        'wins': 0,
+                        'selected': 1
+                    }
+                }
+            }
+        }
+        cache.set('games', games)
 
 class GameView(FormView):
 
