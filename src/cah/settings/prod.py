da917e68c7d8246c243ca1f048f191d0d32127cb
Philip James <pjj@philipjohnjames.com>
2013-04-06 18:18:28 -0700

    Added procfile, prod settings.

---
+++ src/cah/settings/prod.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DATABASES = {
+        'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': 'cah_django.db',
+                'USER': '',
+                'PASSWORD': '',
+                'HOST': 'localhost',
+                'PORT': '',
+        }
+}
