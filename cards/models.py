37af3158768e3f4db3bc08cb4cd9e9c053d88dbd
clach04 <clach04@toofugly.(none)>
2013-04-08 20:33:32 -0700

    Start of cards information in database. Initial data is ONLY 2nd edition. I also snuck in my auto admin code, I wrote this a few years back and appeared to be the easiest way to get an admin interface.

--- cards/models.py
+++ cards/models.py
@@ -10,3 +10,27 @@ class Game(models.Model):
 
     name = models.CharField(max_length = 140)
     game_state = models.CharField(max_length = 140)
+
+
+class BlackCard(models.Model):
+    text = models.CharField(max_length=255)
+    draw = models.SmallIntegerField(default=0)
+    pick = models.SmallIntegerField(default=1)
+    watermark = models.CharField(max_length=5, null=True)
+
+    class Meta:
+        db_table = 'black_cards'
+
+    def __unicode__(self):
+        return self.text
+
+
+class WhiteCard(models.Model):
+    text = models.CharField(max_length=255)
+    watermark = models.CharField(max_length=5, null=True)
+
+    class Meta:
+        db_table = 'white_cards'
+
+    def __unicode__(self):
+        return self.text
