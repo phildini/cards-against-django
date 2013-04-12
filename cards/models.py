c6c5374fd7ae7b085ed0877417503f13033a99ce
Philip James <pjj@philipjohnjames.com>
2013-04-11 22:57:07 -0700

    Updated Game model, added player model.

--- cards/models.py
+++ cards/models.py
@@ -3,21 +3,41 @@
 #
 
 from django.db import models
+from django.contrib.auth.models import User
 
 from jsonfield import JSONField
-
+from model_utils.models import TimeStampedModel
 
 # Create your models here.
 
-class Game(models.Model):
+class Game(TimeStampedModel):
 
     name = models.CharField(max_length=140, unique=True)  # could use pk, but we can use id.
-    game_state = models.CharField(max_length = 140)
-    
+    game_state = models.CharField(max_length=140)
+    is_active = models.BooleanField(default=True)
     gamedata = JSONField()  # See view doc comments
 
     def __unicode__(self):
         # FIXME add game start time, include num players and rounds in display name
+        return self.name
+
+
+class Player(TimeStampedModel):
+
+    # These are the fields I can think of for now, but we're not using it yet,
+    # so we can adjust as needed.
+    name = models.CharField(max_length=140)
+    game = models.ForeignKey(Game)
+    user = models.ForeignKey(
+        User,
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+    player_data = JSONField(blank=True)
+    wins = models.IntegerField(blank=True)
+
+    def __str__(self):
         return self.name
 
 
