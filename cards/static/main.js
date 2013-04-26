dc116be3ba07894d22b40334b96b1e850779e30b
Philip James <philip@eventbrite.com>
2013-04-26 14:28:12 -0700

    Initial drag-and-drop submission code

--- cards/static/main.js
+++ cards/static/main.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
-    $('.draggable').draggable();
+    var numFields = $('.field').length;
+
+    // $('.field').hide();
+
+    for (var i=0; i <numFields; i++) {
+        var element = '<div ' + 'id=' + i + ' class="box droppable">This is a box</div>'
+        $('#submission-area').append(element);
+    }
+
+    $('.draggable').draggable({
+        revert: 'invalid'
+    });
     $('.droppable').droppable({
         drop: function(event, ui) {
             $(this).css("color", "red");
+            $(ui.draggable).css("color", "green");
+            selector = $("label:contains('" + $(ui.draggable).text() + "') > input")
+            // debugger;
+            document.getElementById($(selector)[0].id).checked = true;
+
         }
     });
 });
