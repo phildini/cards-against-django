3d4ff92ac9161a2331d9a2c26ea8b90d69baf756
clach04 <Chris.Clark@actian.com>
2013-04-26 18:57:23 -0700

    Start of logging cleanup. Logging settings are in settings file. Central logger has no config code. NOTE currently logs to /tmp/logfile

--- cah/settings/local.py
+++ cah/settings/local.py
@@ -34,3 +34,58 @@ CACHES = {
 STATIC_ROOT = ''
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ()
+
+############
+SITE_ROOT = '/tmp'  # FIXME loation for log file(s)
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'standard': {
+            #'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'format' : '%(asctime)s %(filename)s:%(lineno)d %(funcName)s %(levelname)s %(message)s',
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+    },
+    'handlers': {
+        'null': {
+            'level':'DEBUG',
+            'class':'django.utils.log.NullHandler',
+        },
+        'logfile': {
+            'level':'DEBUG',
+            'class':'logging.handlers.RotatingFileHandler',
+            'filename': SITE_ROOT + "/logfile",
+            'maxBytes': 50000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console':{
+            'level':'INFO',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'consoledebug':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'WARN',
+        },
+        'django.db.backends': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'cah': {
+            'handlers': ['consoledebug', 'logfile'],
+            'level': 'DEBUG',
+        },
+    }
+}
--- cards/log.py
+++ cards/log.py
@@ -1,8 +1,6 @@
+"""Central logger
+"""
+
 import logging
 
-log_format = '%(asctime)s %(filename)s:%(lineno)d %(funcName)s %(levelname)s %(message)s'
-logging.basicConfig(format=log_format)
-
 logger = logging.getLogger("cah")
-
-logger.setLevel(logging.DEBUG)
