408514e54d0093187c8c54e130b4c4aefe4c368e
clach04 <Chris.Clark@actian.com>
2013-04-26 23:41:49 -0700

    refresh time varies depending on state.

--- cards/views.py
+++ cards/views.py
@@ -127,7 +127,10 @@ class GameView(FormView):
         black_card_id = game.gamedata['current_black_card']
         black_card = BlackCard.objects.get(id=black_card_id)
         context['show_form'] = self.can_show_form()
-        context['refresh_num_secs'] = 10  # FIXME make this either settings variable (or database admin view changeable)
+        if self.game.game_state == GAMESTATE_SELECTION:
+            context['refresh_num_secs'] = 25  # FIXME make this either settings variable (or database admin view changeable)
+        else:
+            context['refresh_num_secs'] = 10  # FIXME make this either settings variable (or database admin view changeable)
         context['game'] = game
         context['black_card'] = black_card.text.replace(BLANK_MARKER, '______')  # FIXME roll this into BlackCard.replace_blanks()
 
