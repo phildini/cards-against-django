a4eedd500c4e2bb98014edd4d39e03e62d11aee9
clach04 <Chris.Clark@actian.com>
2013-05-25 10:12:27 -0700

    Enable saerch for names and text in admin for Cards.

--- cards/admin.py
+++ cards/admin.py
@@ -4,12 +4,33 @@ from django.contrib.admin.sites import AlreadyRegistered
 
 
 def autoregister(*app_list):
+    searchable_fieldnames = ['name', 'text']
     for app_name in app_list:
         app_models = get_app(app_name)
         for model in get_models(app_models):
+            # make fields defined in `searchable_fieldnames` searchable
+            # in the model admin
+            model_admin_name = '%sAdmin' % model.__name__
+            field_names = model._meta.get_all_field_names()
+
+            search_fields = []
+            for field_name in searchable_fieldnames:
+                if field_name in field_names:
+                    search_fields.append(field_name)
+
+            if search_fields:
+                model_admin = type(model_admin_name, (admin.ModelAdmin,),
+                                   {'search_fields': search_fields, })
+            else:
+                model_admin = None
+
             try:
-                admin.site.register(model)
+                if model_admin:
+                    admin.site.register(model, model_admin)
+                else:
+                    admin.site.register(model)
             except AlreadyRegistered:
                 pass
+
 
 autoregister('cards')
