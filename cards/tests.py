2426387fdc8f65380c874012f438f5ccc018fe61
Philip James <pjj@philipjohnjames.com>
2013-06-30 14:34:53 -0700

    Began the process of testing, added @mralex to the credits.

--- cards/tests.py
+++ cards/tests.py
@@ -10,6 +10,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from cards.models import Game
 
 
 class SimpleTest(TestCase):
@@ -17,3 +18,12 @@ class SimpleTest(TestCase):
     def test_basic_addition(self):
         """Tests that 1 + 1 always equals 2."""
         self.assertEqual(1 + 1, 2)
+
+class LobbyViewTests(TestCase):
+    def test_game_list(self):
+        game_1 = Game.objects.create(name='Test')
+        resp = self.client.get('/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('joinable_game_list' in resp.context)
+        self.assertEqual(resp.context['joinable_game_list'][0][1], 'Test')
+
