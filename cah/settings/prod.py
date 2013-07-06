545456e8a1da5bd4b54840ad5b5d34c671010bc7
Philip James <pjj@philipjohnjames.com>
2013-07-06 11:12:55 -0700

    Updated prod settings.

--- cah/settings/prod.py
+++ cah/settings/prod.py
@@ -13,6 +13,10 @@ ALLOWED_HOSTS = ['.thisisnotthatgame.com','.herokuapp.com', 'localhost', '127.0.
 
 STATIC_URL = 'http://philipjohnjames.com/applesanon/static/'
 
+TEMPLATE_DIRS = (
+    PROJECT_ROOT.child('templates'),
+)
+
 USE_PUSHER = True
 PUSHER_APP_ID = get_env_variable("PUSHER_APP_ID")
 PUSHER_KEY = get_env_variable("PUSHER_KEY")
