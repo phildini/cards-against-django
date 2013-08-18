dc0f2d680d9c3fb3df76d9724e5581b29f9088ac
Alex Roberts <alex@redprocess.com>
2013-08-17 21:08:48 -0700

    Setup virtualenv and install requirements on provision.

--- puppet/modules/python/manifests/init.pp
+++ puppet/modules/python/manifests/init.pp
@@ -1,15 +1,53 @@
 class python {
+    $root_dir = "/home/vagrant"
+    $venv_dir = "${ root_dir }/venv"
+    $src_dir = "${ root_dir }/src"
+
     package { "python-pip":
-        ensure => 'latest',
+        ensure => "latest",
     }
 
     package { "python-virtualenv":
-        ensure => 'latest',
+        ensure => "latest",
     }
 
     package { "virtualenvwrapper":
-        ensure => 'latest',
-        provider => 'pip',
-        require => Package['python-pip'],
+        ensure => "latest",
+        provider => "pip",
+        require => Package["python-pip"],
+    }
+
+    exec { "virtualenv":
+        command => "mkdir -p ${ venv_dir } && virtualenv ${ venv_dir }",
+        path => [ "/bin", "/usr/bin", "/usr/sbin", "/usr/local/bin" ],
+        cwd => "/tmp",
+        creates => "${ venv_dir }/bin/activate",
+        user => "vagrant",
+    }
+
+    $base_requirements = "${ src_dir }/requirements/_base.txt"
+    $requirements = "${ src_dir }/requirements/local.txt"
+
+    file { $base_requirements:
+        ensure => present,
+        audit => content,
+        replace => false,
+    }
+
+    file { $requirements:
+        ensure => present,
+        audit => content,
+        replace => false,
+    }
+
+    exec { "pip":
+        provider => "shell",
+        command => "${ venv_dir }/bin/pip install -r $requirements",
+        timeout => 1800,
+        user => "vagrant",
+        subscribe => [
+            File[$base_requirements],
+            File[$requirements],
+        ],
     }
 }
