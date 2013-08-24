15d788ce2ea0b950846f88b900d1c0ae06b4f100
Philip James <pjj@philipjohnjames.com>
2013-08-23 21:01:32 -0700

    Added better long polling as a fallback for the push.

--- cards/static/main.js
+++ cards/static/main.js
@@ -1,58 +1,29 @@
-$(function(){
-    var numFields = $('.field').length;
-
-    $('.field li').addClass('choice');
-
-    // for (var i=0; i <numFields; i++) {
-    //     var element = '<div ' + 'id=' + i + ' class="box droppable">This is a box</div>'
-    //     $('#submission-area').append(element);
-    // }
-
-    // $('.draggable').draggable({
-    //     revert: 'invalid'
-    // });
-    // $('.droppable').droppable({
-    //     drop: function(event, ui) {
-    //         $(this).css("color", "red");
-    //         $(ui.draggable).css("color", "green");
-    //         var selection_html = $(ui.draggable).html();
-    //         var selection_text;
-    //         if (selection_html.toLowerCase().indexOf('<strong>') > 1) {
-    //             selection_text = $(ui.draggable).find('strong').first().contents().text();
-    //         }
-    //         else {
-    //             selection_text = $(ui.draggable).text();
-    //         }
-    //         selector = $("label:contains('" + selection_text + "') > input")
-    //         // debugger;
-    //         document.getElementById($(selector)[$(this)[0].id].id).checked = true;
-    //         $(this).droppable('option', 'accept', ui.draggable);
-
-    //     }
-    // });
-
-    window.LongPolling = (function($) {
-        var _timeout = null,
-            _options = {},
-            doLongPoll = function() {
-                $.ajax({
-                    url: '/game/' + _options.gameId + '/checkready',
-                    data: {}
-                }).done(function(res) {
-                    if (res.isReady) {
-                        window.location.reload(true);
+window.LongPolling = (function($) {
+    var _timeout = null,
+        _options = {},
+        doLongPoll = function() {
+            $.ajax({
+                url: '/game/' + _options.gameId + '/api',
+                crossDomain: true
+            }).done(function(res) {
+                    var state = $(".data").data();
+                    if (res.game_state != state.game_state) {
+                        location.reload(true);
                     }
-                });
-            };
-
-        return {
-            startLongPolling: function(options) {
-                _options = $.extend({}, options);
-                _timeout = window.setInterval(doLongPoll, 5000);
-            },
-            stopLongPolling: function() {
-                window.clearInterval(_timeout);
-            }
+                    if (res.gamedata.round != state.round) {
+                        location.reload(true);
+                    }
+                }
+            );
         };
-    }(jQuery));
-});
+
+    return {
+        startLongPolling: function(options) {
+            _options = $.extend({}, options);
+            _timeout = window.setInterval(doLongPoll, 30000);
+        },
+        stopLongPolling: function() {
+            window.clearInterval(_timeout);
+        }
+    };
+}(jQuery));
