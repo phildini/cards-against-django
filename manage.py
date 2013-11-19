b7109fb831fcae14ce205e5740e5c429d18231b6
clach04 <Chris.Clark@actian.com>
2013-11-18 21:00:07 -0800

    Avoid unhelpful messages with old Python versions. Without this change gand using Python 2.6.4 get:
    	TypeError: Error when calling the metaclass bases
    	    __init__() keywords must be strings
    
    With traceback enabled get:
    
        django\db\models\base.py .... in __new__
                new_class.add_to_class('_meta', Options(meta, **kwargs))
    	        TypeError: Error when calling the metaclass bases
    		        __init__() keywords must be strings

--- manage.py
+++ manage.py
@@ -7,6 +7,9 @@ import os
 import sys
 
 if __name__ == "__main__":
+    if sys.version_info < (2, 6, 5):
+        # Avoid unhelpful errors https://code.djangoproject.com/ticket/18961
+        raise NotImplementedError('Python version is too old (for Django)')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cah.settings")
 
     from django.core.management import execute_from_command_line
